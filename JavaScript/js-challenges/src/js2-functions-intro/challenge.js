c637cbfdde76506daa37d433951760b872f54aa5
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-06-12 22:46:24 +1000

    complete js2 challenge

--- JavaScript/js-challenges/src/js2-functions-intro/challenge.js
+++ JavaScript/js-challenges/src/js2-functions-intro/challenge.js
@@ -17,7 +17,7 @@
  * @returns {string} John Smith
  */
 export const createFullName = (firstName, lastName) => {
-  /* Write your code here */
+  return firstName + " " + lastName;
 };
 
 /**
@@ -28,8 +28,14 @@ export const createFullName = (firstName, lastName) => {
  * @param {number} number2 200
  * @returns {number} 100
  */
-export const findSmallestNumber = (/* Write the parameters here */) => {
-  /* Write your code here */
+export const findSmallestNumber = (num1, num2) => {
+  let smallest = 0;
+  if (num1 < num2) {
+    smallest = num1;
+  } else {
+    smallest = num2;
+  }
+  return smallest;
 };
 
 /**
@@ -40,8 +46,9 @@ export const findSmallestNumber = (/* Write the parameters here */) => {
  * @param {number} number2 6
  * @returns {number} 18
  */
-export const multiplyNumbers = (/* Write the parameters here */) => {
-  /* Write your code here */
+export const multiplyNumbers = (num1, num2) => {
+  const product = num1 * num2;
+  return product;
 };
 
 /* Intermediate Challenges */
@@ -57,7 +64,13 @@ export const multiplyNumbers = (/* Write the parameters here */) => {
  * @returns {string} "You got a new high score!" | "So close!" | "Better luck next time!"
  */
 export const checkIfNewHighScore = (score, highScore) => {
-  /* Write your code here */
+  if (score > highScore) {
+    return "You got a new high score!";
+  } else if (score === highScore) {
+    return "So close!";
+  } else {
+    return "Better luck next time!";
+  }
 };
 
 /**
@@ -67,7 +80,8 @@ export const checkIfNewHighScore = (score, highScore) => {
  * @returns {string} "15 degrees celsius is 59 degrees fahrenheit"
  */
 export const celsiusToFahrenheit = (tempInCelsius) => {
-  /* Write your code here */
+  const tempInFahrenheit = (tempInCelsius * 9) / 5 + 32;
+  return `${tempInCelsius} degrees celsius is ${tempInFahrenheit} degrees fahrenheit`;
 };
 
 /**
@@ -80,7 +94,10 @@ export const celsiusToFahrenheit = (tempInCelsius) => {
  * @returns {number} 47450
  */
 export const calculateLifetimeSupply = (snickersPerDay, age, maxAge) => {
-  /* Write your code here */
+  const yearsRemain = maxAge - age;
+  const daysRemain = yearsRemain * 365;
+  const snickersNeeded = snickersPerDay * daysRemain;
+  return snickersNeeded;
 };
 
 /* Advanced Challenges */
@@ -100,7 +117,23 @@ export const calculateLifetimeSupply = (snickersPerDay, age, maxAge) => {
  * @returns {string} A - F | Score unavailable
  */
 export const getGrade = (score) => {
-  /* Write your code here */
+  if (typeof score !== "number" || isNaN(score)) {
+    return "Score unavailable";
+  } else if (score >= 80 && score <= 100) {
+    return "A";
+  } else if (score >= 70 && score < 80) {
+    return "B";
+  } else if (score >= 60 && score < 70) {
+    return "C";
+  } else if (score >= 50 && score < 60) {
+    return "D";
+  } else if (score >= 40 && score < 50) {
+    return "E";
+  } else if (score >= 0 && score < 40) {
+    return "F";
+  } else {
+    return "Score unavailable";
+  }
 };
 
 /**
@@ -110,7 +143,8 @@ export const getGrade = (score) => {
  * @returns {number} 28.27
  */
 export const calculateAreaOfCircle = (radius) => {
-  /* Write your code here */
+  const area = Math.PI * radius ** 2; // = pi * r^2
+  return Number(area.toFixed(2));
 };
 
 /* Expert Challenge */
@@ -131,5 +165,22 @@ export const calculateAreaOfCircle = (radius) => {
  * @param {string} name John
  */
 export const getStudentSummary = (score, name) => {
-  /* Write your code here */
+  const grade = getGrade(score);
+  let result = "";
+  if (grade === "A") {
+    result = `Congratulations ${name}! You achieved a grade of A.`;
+  } else if (grade === "B") {
+    result = `Well done ${name}! You achieved a grade of B.`;
+  } else if (grade === "C") {
+    result = `Nicely done ${name}! You achieved a grade of C.`;
+  } else if (grade === "D") {
+    result = `That's okay ${name}. You achieved a grade of D.`;
+  } else if (grade === "E") {
+    result = `Too bad ${name}. You achieved a grade of E.`;
+  } else if (grade === "F") {
+    result = `Sorry ${name}. You achieved a grade of F. There's always next year.`;
+  } else {
+    result = `My apologies ${name}, there's been an error in processing your grade.`;
+  }
+  return result;
 };
