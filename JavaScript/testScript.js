2fc42d72fc82f8e70377c2c25a85e1d717838c00
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-06-16 11:50:16 +1000

    Update testScript.js

--- JavaScript/testScript.js
+++ JavaScript/testScript.js
@@ -1,6 +1,10 @@
-const numberArr = [];
-let sum = 0;
-let average = Math.round(sum / numberArr.length);
-console.log(numberArr.length);
-console.log(average);
-console.log(0/0);
+// const numberArr = [];
+// let sum = 0;
+// let average = Math.round(sum / numberArr.length);
+// console.log(numberArr.length);
+// console.log(average);
+// console.log(0/0);
+
+
+const myArr = [1, 2, [3, [4, 5, 6], 7], 8];
+console.log(myArr.flat(2));
