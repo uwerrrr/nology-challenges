66f4aa455429518eddbccf66fe46d850d5ba6356
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-06-22 13:20:48 +1000

    add fetch codealong challenge

---
+++ JavaScript/API/Fetch/scripts.js
@@ -0,0 +1,10 @@
+/* # Countries API
+
+Have a look at the documentation for [Countries API](https://restcountries.com/)
+
+-   Create an interface with an input and a button
+-   When the user clicks the button, country info gets displayed on the page
+-   Get the country from the input
+-   Use async/await over .then
+ */
+
